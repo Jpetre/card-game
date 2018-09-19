1102eb9fe4171b8186876a59c31e34d09c16e61f
Jpetre <jeremy.petre@gmail.com>
2018-09-19 20:54:30 +0200

    core (move all datas to reducers, no more state)

--- src/store/actions/boardActions.js
+++ src/store/actions/boardActions.js
@@ -1,6 +1,13 @@
-export const endTurn = turn => dispatch => {
+export const endTurn = payload => dispatch => {
   dispatch({
     type: 'TURN_NUMBER',
-    payload: turn
+    payload
+  })
+}
+
+export const dragEndSuccess = payload => dispatch => {
+  dispatch({
+    type: 'DRAG_END_SUCCESS',
+    payload
   })
 }
--- src/store/reducers/boardReducer.js
+++ src/store/reducers/boardReducer.js
@@ -1,6 +1,18 @@
+import { hands } from '../mocks/hands';
+
 const initialState = {
-  turn: 1,
-  manaPool: [1, 1, 1, 1]
+  turnNumber: 1,
+  manaPool: [1, 1, 1, 1],
+  turnPlayerId: 0,
+  turnPlayer: hands[0].player,
+  playerOneHand: hands[0].cards,
+  playerTwoHand: hands[1].cards,
+  playerThreeHand: hands[2].cards,
+  playerFourHand: hands[3].cards,
+  playerOneFz: [],
+  playerTwoFz: [],
+  playerThreeFz: [],
+  playerFourFz: [],
 };
 
 export default (state = initialState, action) => {
@@ -8,14 +20,23 @@ export default (state = initialState, action) => {
     case 'TURN_NUMBER':
       return {
         ...state,
-        turn: action.payload,
-        manaPool: [action.payload, action.payload, action.payload, action.payload]
+        turnNumber: action.payload.nextTurnNumber,
+        turnPlayerId: action.payload.nextTurnPlayerId,
+        turnPlayer: hands[action.payload.nextTurnPlayerId].player,
+        manaPool: [action.payload.nextTurnNumber, action.payload.nextTurnNumber, action.payload.nextTurnNumber, action.payload.nextTurnNumber]
       }
 
-    case 'PLAYED_CARD':
+    case 'DRAG_END_SUCCESS':
       return {
         ...state,
-        manaPool: action.payload
+        playerOneHand: action.payload.playerOneHand,
+        playerTwoHand: action.payload.playerTwoHand,
+        playerThreeHand: action.payload.playerThreeHand,
+        playerFourHand: action.payload.playerFourHand,
+        playerOneFz: action.payload.playerOneFz,
+        playerTwoFz: action.payload.playerTwoFz,
+        playerThreeFz: action.payload.playerThreeFz,
+        playerFourFz: action.payload.playerFourFz,
       }
     default:
     return state
